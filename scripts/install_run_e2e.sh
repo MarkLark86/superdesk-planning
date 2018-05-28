3eed827e8a2c42972c9161fb796065ead7d71371
Mark Pittaway <mark.pittaway@gmail.com>
2018-05-28 13:50:25 +1000

    Don't install npm packages in client folder

--- scripts/install_run_e2e.sh
+++ scripts/install_run_e2e.sh
@@ -16,20 +16,19 @@ sed -i -re 's/("superdesk-core":)[^,]*(,?)/\1 "file:..\/client-core"\2/' client/
 git clone https://github.com/superdesk/superdesk-core.git server-core
 git clone https://github.com/superdesk/superdesk-client-core.git client-core
 
-cd $E2E_DIR
 sed -i 's/.*superdesk-planning.git.*/-e ..\/..\//' server/requirements.txt
 sed -i -re 's/("superdesk-planning":)[^.]*(,?)/\1 "file:..\/..\/"\2/' client/package.json
 cat server/requirements.txt
 cat client/package.json
 
 
-# git checkout planning
-#npm install --python=python2.7
+npm install --python=python2.7
 npm install -g grunt-cli
 export DISPLAY=:99.0 && /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1920x1080x24
 export CHROME_BIN=`which google-chrome` && $CHROME_BIN --version ;
 cd server && pip install -U -r dev-requirements.txt && cd ..
-cd client && npm install --python=python2.7 && grunt build && cd ..
+# cd client && npm install --python=python2.7 && grunt build && cd ..
+cd client && grunt build && cd ..
 pwd
 ls -la
 ls -la $E2E_DIR

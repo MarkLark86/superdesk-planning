763287cf3508d42e88ec511acfdd48976d0bb6d8
Mark Pittaway <mark.pittaway@gmail.com>
2018-05-28 12:26:25 +1000

    Manually clone superdesk-client-core and superdesk-core repos

--- scripts/install_run_e2e.sh
+++ scripts/install_run_e2e.sh
@@ -2,20 +2,37 @@
 BACKEND_DIR=server
 VENV=`pwd`/$BACKEND_DIR/env/bin/activate
 PLANNING_DIR=`pwd`
-mkdir e2e && cd e2e
-git clone https://github.com/superdesk/superdesk-aap.git
-cd superdesk-aap
-# git checkout planning-mvp
-git checkout planning
+E2E_DIR=`pwd`/e2e
+mkdir $E2E_DIR && cd $E2E_DIR
+
+git init
+git remote add origin https://github.com/superdesk/superdesk.git
+git fetch origin planning-mvp
+git checkout planning-mvp
+
+sed -i 's/.*superdesk-core.git.*/-e ..\/server-core/' server/requirements.txt
+sed -i -re 's/("superdesk-core":)[^,]*(,?)/\1 "file:..\/client-core"\2/' client/package.json
+
+git clone https://github.com/superdesk/superdesk-core.git server-core
+git clone https://github.com/superdesk/superdesk-client-core.git client-core
+
+cd $E2E_DIR
+sed -i 's/.*superdesk-planning.git.*/-e $PLANNING_DIR/' server/requirements.txt
+sed -i -re 's/("superdesk-planning":)[^.]*(,?)/\1 "file:$PLANNING_DIR"\2/' client/package.json
+cat server/requirements.txt
+cat client/package.json
+
+
+# git checkout planning
 npm install --python=python2.7
 npm install -g grunt-cli
 export DISPLAY=:99.0 && /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1920x1080x24
 export CHROME_BIN=`which google-chrome` && $CHROME_BIN --version ;
-cd server && pip install -r requirements.txt && cd ..
+cd server && pip install -U -r dev-requirements.txt && cd ..
 cd client && npm install && grunt build && cd ..
 pwd
 ls -la
-ls -la $PLANNING_DIR/e2e/
+ls -la $E2E_DIR
 ls -la $PLANNING_DIR
 sudo sed -i 's\enabled: true\enabled: false\' /etc/mongod.conf
 sudo service mongod restart
@@ -26,9 +43,9 @@ sudo service mongod restart
 sudo service elasticsearch restart
 sleep 60
 #curl -XPUT 'http://localhost:9200/_snapshot/backups' -d '{"type": "fs", "settings": {"location": "/tmp/es-backups"}}'
-cd $PLANNING_DIR/e2e/superdesk-aap/client/dist
+cd $E2E_DIR/client/dist
 nohup python -m http.server 9000 &
-cd $PLANNING_DIR/e2e/superdesk-aap/server
+cd $E2E_DIR/server
 echo "SUPERDESK_TESTING = True" | sudo tee -a ./settings.py
 echo "DEBUG = True" | sudo tee -a ./settings.py
 # echo "MONGO_DBNAME = 'superdesk_e2e'" | sudo tee -a ./settings.py

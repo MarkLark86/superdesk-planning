7b96275532e94706e6fc50fdc5715f4d8da8f4fa
Mark Pittaway <mark.pittaway@gmail.com>
2018-05-29 09:03:40 +1000

    Link Superdesk to Planning module in development mode

--- scripts/install_run_e2e.sh
+++ scripts/install_run_e2e.sh
@@ -11,20 +11,29 @@ git remote add origin https://github.com/superdesk/superdesk.git
 git fetch origin master
 git checkout master
 
-# Delete analytics and publisher from config
+# Copy config files to enable Planning
 cp -f $SCRIPTS_DIR/package.json client/package.json
 cp -f $SCRIPTS_DIR/superdesk.config.js client/superdesk.config.js
 
 cp -f $SCRIPTS_DIR/requirements.txt server/requirements.txt
 cp -f $SCRIPTS_DIR/settings.py server/settings.py
 
-cd client
+echo "Installing Planning npm modules"
+cd $PLANNING_DIR
 npm --python=python2.7 install
+
+echo "Install Superdesk npm modules"
+cd $E2E_DIR/client
+npm --python=python2.7 install
+echo "Linking Superdesk to local Planning module"
 npm --python=python2.7 link ../../
 
-cd ../server
+echo "Installing Superdesk pip modules"
+cd $E2E_DIR/server
 pip install -r requirements.txt
-cd ../../
+
+echo "Install Planning pip modules (in development mode)"
+cd $PLANNING_DIR
 pip install -e .
 
 export DISPLAY=:99.0 && /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1920x1080x24

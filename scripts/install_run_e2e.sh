f7f575771107b67471728845d29ac8278850bf41
Mark Pittaway <mark.pittaway@gmail.com>
2018-05-29 09:13:12 +1000

    Add 'grunt build' to build script

--- scripts/install_run_e2e.sh
+++ scripts/install_run_e2e.sh
@@ -18,31 +18,44 @@ cp -f $SCRIPTS_DIR/superdesk.config.js client/superdesk.config.js
 cp -f $SCRIPTS_DIR/requirements.txt server/requirements.txt
 cp -f $SCRIPTS_DIR/settings.py server/settings.py
 
+echo
+echo "--------------------------------------------------------------------------------------------"
 echo "Installing Planning npm modules"
 cd $PLANNING_DIR
 npm --python=python2.7 install
 
+echo
+echo "--------------------------------------------------------------------------------------------"
 echo "Install Superdesk npm modules"
 cd $E2E_DIR/client
 npm --python=python2.7 install
+
+echo
+echo "--------------------------------------------------------------------------------------------"
 echo "Linking Superdesk to local Planning module"
 npm --python=python2.7 link ../../
 
+echo
+echo "--------------------------------------------------------------------------------------------"
 echo "Installing Superdesk pip modules"
 cd $E2E_DIR/server
 pip install -r requirements.txt
 
+echo
+echo "--------------------------------------------------------------------------------------------"
 echo "Install Planning pip modules (in development mode)"
 cd $PLANNING_DIR
 pip install -e .
 
+echo
+echo "--------------------------------------------------------------------------------------------"
+echo "Build static HTML/JS files"
+cd $E2E_DIR/client
+grunt build
+
 export DISPLAY=:99.0 && /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1920x1080x24
 export CHROME_BIN=`which google-chrome` && $CHROME_BIN --version ;
 
-pwd
-ls -la
-ls -la $E2E_DIR
-ls -la $PLANNING_DIR
 sudo sed -i 's\enabled: true\enabled: false\' /etc/mongod.conf
 sudo service mongod restart
 
